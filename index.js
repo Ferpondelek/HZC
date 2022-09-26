a22b6d22ed43e35d6e61db260769bc7cb6481e70
Ferpondelek <104093109+Ferpondelek@users.noreply.github.com>
2022-09-26 19:10:24 -0300

    página home finalizada

---
+++ index.js
@@ -0,0 +1,7 @@
+const bottaoMenu = document.querySelector('.cabecalho__menu');
+const menu = document.querySelector('.menu-lateral');
+
+
+bottaoMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu-lateral--ativo')
+})
